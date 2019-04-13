590cd16d3645fdde4fda012b5bdf9637d4ff7aad
mayee007 <yelekeri@gmail.com>
2019-04-12 23:29:57 -0400

    junit test cases added

---
+++ src/test/java/com/mine/testNG/junit/Test1.java
@@ -0,0 +1,18 @@
+package com.mine.testNG.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void test1() {
+		System.out.println("inside Junit 1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("inside Junit 2");
+	}
+}
